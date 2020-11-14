6018aeafd41cb4746e09b8d271cc58c48529635d
emster7013 <emily@msn.com>
2020-11-13 20:43:30 -0500

    Backend configuration, adding in the validation and need to add in the material UI

--- my-app/src/Contact/ContactPage.js
+++ my-app/src/Contact/ContactPage.js
@@ -1,36 +1,64 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import Amplify, {API} from 'aws-amplify';
 import { useForm } from 'react-hook-form';
 import Button from '@material-ui/core/Button';
  
 {/*Here I'm going to redo the whole init vaules and errors so that we can get the form validation working the right way*/}
- 
+const initialValues = {
+	name: '',
+	email: '',
+	message: ''
+};
+
+const errors = {
+	nameError: '',
+	emailError: '',
+	messageError: ''
+};
 const ContactPage = (props) => {
-  const [data, setData] = useState({
-    firstName: '',
-    email: '',
-    message: ''
-  });
-  
-  
+    const [formValues, setFormValues] = useState(initialValues);
+    
+ const handleSubmit = e => {
+     e.preventDefault();
+     const data = {
+         body : {
+        name:formValues.name,
+        email:formValues.email,
+        message:formValues.message
+    }
+}
+     API.post('mkapi', '/sendemail', data)
+     .then((res)=>{
+         console.log(data)
+     })
+     setFormValues(initialValues)
+ }
+ const handleClear = e => {
+    e.preventDefault();
+    setFormValues(initialValues);
+};
   const handleChange = (e) => {
-    setData({
-      ...data,
+    setFormValues({
+      ...formValues,
       [e.target.name]: e.target.value,
     });
   };
- 
+//   (if name === '' ''){
+//     //Throw some kind of error
+//     }else{
+//     name = name.target 
+
   return (
     <div className='contact'>
         <h2>Contact Us!</h2>
         <p>Let us know what you think! In order to provide you with better service please don't heistate to give use your feedback. Thank you.</p>
-      <form onSubmit={handleChange}>
+      <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor='firstName'>First Name</label>
           <input
-            name='firstName'
+            name='name'
             type='text'
-            value={data.firstName}
-            placeholder='First Name'
+            value={formValues.name}
+            placeholder='Name'
             onChange={handleChange}
           />
          
@@ -38,30 +66,26 @@ const ContactPage = (props) => {
  
  
         <div>
-          <label htmlFor='email'>
-            Email
-          </label>
           <input
             name='email'
             type='email'
-            value={data.email}
+            value={formValues.email}
             placeholder='email'
             onChange={handleChange}
           />
           {/*This is where I'll want to somehow to the email portion of the submit thingy*/}
         </div>
         <div>
-          <label htmlFor='message'>Message</label>
           <input
             name='message'
             type='text'
-            value={data.message}
+            value={formValues.message}
             placeholder='Message'
             onChange={handleChange}
           />
         </div>
         {/* <Button type='submit'  variant='contained' color='primary'> </Button> */}
-        <button type ='submit'> Submit</button>
+        <button type ='submit' onClick={handleSubmit}> Submit</button>
       </form>
     </div>
   );
